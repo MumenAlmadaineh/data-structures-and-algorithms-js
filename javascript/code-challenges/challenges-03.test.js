751710c7679561a9296fa6e13b4602b34f801235
MumenAlmadaineh <mumen_almadaineh@outlook.com>
2021-09-13 00:11:08 +0300

    Fix filter

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -181,7 +181,6 @@ TESTS
 All the code below will verify that your functions are working to solve the challenges.
 DO NOT CHANGE any of the below code.
 Run your tests from the console: jest challenges-08.test.js
-
 ------------------------------------------------------------------------------------------------ */
 
 describe('Testing challenge 1', () => {
